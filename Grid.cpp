f87caaa7516285081264d49fcfcfd16ad6b12a9c
freekdh <freekdh@gmail.com>
2015-05-11 19:23:47 +0200

    g

---
+++ Grid.cpp
@@ -0,0 +1,14 @@
+#include "Grid.h"
+
+Grid::Grid(int x; int y; int value)
+{
+x_size = x;
+y_size = y;
+value_size = value;
+}
+
+Grid::Set(int x; int y; int i)
+{
+
+}
+
---
+++ Grid.h
@@ -0,0 +1,16 @@
+#ifndef GRID_H
+#define GRID_H
+
+class Grid
+{
+    public:
+        Grid(int x; int y; int value);
+        void Set(int x; int y; int i);
+        void Get(int )
+        void Next
+
+        private:
+    int coordinate[int x_size][int y_size][int value_size];
+};
+
+#endif // GRID_H
