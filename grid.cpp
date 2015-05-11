10e5fe1ec7d60cf49fb55e6fc1bc96f5a85ef75a
richelbilderbeek <richel@richelbilderbeek.nl>
2015-05-11 19:58:46 +0200

    Added rules

--- grid.cpp
+++ grid.cpp
@@ -47,6 +47,11 @@ int Grid::GetWidth() const
 void Grid::Next()
 {
   //Game Of Life here
+  //
+  // Any live cell with fewer than two live neighbours dies
+  // Any live cell with two or three live neighbours lives
+  // Any live cell with more than three live neighbours dies
+  // Any dead cell with exactly three live neighbours becomes a live cell
 
   //STUB:
   const int x{std::rand() % GetWidth()};
