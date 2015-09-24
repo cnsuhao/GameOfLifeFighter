3c19386192aa97cf7f72c73b6b734fc8cb43ed2d
Frank Smit <frank@61924.nl>
2015-09-24 23:28:04 +0200

    glgolf: Render Game of Life with nanovg.

--- glgolf/window.go
+++ glgolf/window.go
@@ -1,6 +1,7 @@
 package glgolf
 
 import (
+	"fmt"
 	"github.com/FSX/nanovg"
 	"github.com/FSX/nanovg/gl"
 	"github.com/veandco/go-sdl2/sdl"
@@ -63,9 +64,28 @@ func StartGame() {
 	context := initGL(window)
 	nvg := initNanovg()
 
+	ww, wh := window.GetSize()
+	gw := ww / 5
+	gh := wh / 5
+	fmt.Println(gw, gh)
+
+	life := NewLife(gw, gh)
+
+	refgrid := make([][]bool, gh)
+	for i := range refgrid {
+		refgrid[i] = make([]bool, gw)
+	}
+
 	running := true
+	minms := uint32(1000 / 15)
+	fmt.Println(minms)
+
+	gl.ClearColor(0.3, 0.3, 0.32, 1.0)
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT)
 
 	for running {
+		lt := sdl.GetTicks()
+
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			switch t := event.(type) {
 			case *sdl.QuitEvent:
@@ -77,29 +97,23 @@ func StartGame() {
 			}
 		}
 
-		ww, wh := window.GetSize()
+		life.Step()
 
+		ww, wh := window.GetSize()
 		gl.Viewport(0, 0, ww, wh)
-		gl.ClearColor(0.3, 0.3, 0.32, 1.0)
-		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT)
 
 		nvg.BeginFrame(ww, wh, 1.0)
 
-		nvg.BeginPath()
-
-		for i := float32(10.0); i <= 600.0; i += 10.0 {
-			nvg.MoveTo(i, 10.0)
-			nvg.LineTo(i, 600.0)
-			nvg.MoveTo(10.0, i)
-			nvg.LineTo(600.0, i)
-		}
-
-		nvg.StrokeColor(nanovg.RGBA(255, 255, 255, 255))
-		nvg.StrokeWidth(1.0)
-		nvg.Stroke()
-		nvg.ClosePath()
+		drawGrid(life, nvg, refgrid)
 
 		nvg.EndFrame()
+
+		if d := sdl.GetTicks() - lt; d < minms {
+			fmt.Println("Fast", d)
+			sdl.Delay(minms - d)
+		} else {
+			fmt.Println("Slow", d)
+		}
 
 		sdl.GL_SwapWindow(window)
 	}
@@ -109,4 +123,38 @@ func StartGame() {
 	sdl.GL_DeleteContext(context)
 	nvg.Close()
 	sdl.Quit()
+}
+
+func drawGrid(life *Life, nvg *nanovg.Context, refgrid [][]bool) {
+	w, h := float32(life.w), float32(life.h)
+	counter := 0
+	liveCounter := 0
+	deadCounter := 0
+
+	for y := float32(0); y < h; y++ {
+		for x := float32(0); x < w; x++ {
+			a, b := int(x), int(y)
+			c := life.a.s[b][a]
+
+			if refgrid[b][a] != c {
+				nvg.BeginPath()
+				nvg.Rect(x*5+1, y*5+1, 3, 3)
+
+				if c {
+					liveCounter++
+					nvg.FillColor(nanovg.RGBA(255, 255, 255, 255))
+				} else {
+					nvg.FillColor(nanovg.RGBA(100, 100, 100, 255))
+					deadCounter++
+				}
+
+				nvg.Fill()
+				nvg.ClosePath()
+
+				counter++
+			}
+
+			refgrid[b][a] = c
+		}
+	}
 }
